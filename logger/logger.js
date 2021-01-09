b6047017fd7e86ca9a8c4271adc754b737907723
payalghusalikar <payal.ghusalikar9@gmail.com>
2021-01-10 01:58:59 +0530

    refactor : connect database using mongoDB instance

--- logger/logger.js
+++ logger/logger.js
@@ -9,9 +9,9 @@ const logger = createLogger({
     transports: [
         new transports.File({
             
-            filename: (`../../log/error.log`),
+            //filename: (`../../log/error.log`),
             filename: (`../../log/info.log`),
-            filename: (`../../log/warn.log`),
+           // filename: (`../../log/warn.log`),
             level: `info`,
             format: winston.format.combine(format.timestamp(), format.json())
         }),
